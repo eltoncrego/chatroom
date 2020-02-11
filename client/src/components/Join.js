858971b1f782e9f10d402463800685901f968c8f
Elton C. Rego <rego.elton@gmail.com>
2020-02-10 20:39:23 -0800

    Init basic structure of join page

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {Link} from 'react-router-dom';
 
-const Join = () => (
-  <h1>Join.</h1>
-);
+const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return(
+    <div className='join'>
+      <div className='join-container'>
+        <h1 className='join__heading'>Join</h1>
+        <div>
+          <input 
+            placeholder='' 
+            className='join__input' 
+            type='text' 
+            onChange={(event) => {
+              setName(event.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <input 
+            placeholder='' 
+            className='join__input element--margin-16' 
+            type='text'
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}/>
+        </div>
+        <Link 
+          onClick={event => (!name || !room) ? event.preventDefault() : null}
+          to={`/chat?name=${name}&room=${room}`}
+        >
+          <button className="button element--margin-16" type="submit">Sign In</button>
+        </Link>
+      </div>
+    </div>
+  );
+}
 
 export default Join;
