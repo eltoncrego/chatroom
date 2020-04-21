e935805ba2307eaf5fd2e3c98d8c52f7b254c391
Elton C. Rego <rego.elton@gmail.com>
2020-04-21 09:00:01 -0700

    Reorg server code

--- api/src/index.js
+++ api/src/index.js
@@ -4,41 +4,39 @@ const http = require('http');
 
 const PORT = process.env.PORT || 5000;
 const router = require('./router');
+const { addUser, getUser } = require('./users');
 
 const handler = express();
 const server = http.createServer(handler);
 const io = socketio(server);
 
-const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-
 io.on('connection', (socket) => {
   console.log(`${socket.id}: New Connection Instance`);
-  socket.on('join', (params, callback) => doJoin(socket, params, callback));
+
+  socket.on('join', ({ name, room }, callback) => {
+    if (!name || !room) {
+      callback({ error: `${socket.id}: Either Name of User or Room is Undefined on Server-Side`});
+      return;
+    }
+    const { error, user } = addUser({ id: socket.id, name, room });
+    console.log(user);
+    if (error) {
+      callback(error);
+      return;
+    }
+    socket.emit('message', { user: 'Troop Coordinator', text: `Welcome, ${user.name}, to ${user.room}`});
+    socket.broadcast.to(user.room).emit('message', { user: 'Troop Coordinator', text: `${user.name}, has joined the troop!`})
+    socket.join(user.room);
+  });
+  
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
     io.to(user.room).emit('message', { user: user.name, text: message });
     callback();
   });
+
   socket.on('disconnect', () => console.log(`${socket.id}: A Connection Instance Has Ended`));
 });
 
-function doJoin(socket, { name, room }, callback) {
-  if (!name || !room) {
-    callback({ error: `${socket.id}: Either Name of User or Room is Undefined on Server-Side`});
-    return;
-  }
-
-  const { error, user } = addUser({ id: socket.id, name, room });
-  console.log(user);
-  if (error) {
-    callback(error);
-    return;
-  }
-
-  socket.emit('message', { user: 'Troop Coordinator', text: `Welcome, ${user.name}, to ${user.room}`});
-  socket.broadcast.to(user.room).emit('message', { user: 'Troop Coordinator', text: `${user.name}, has joined the troop!`})
-  socket.join(user.room);
-}
-
 handler.use(router);
-server.listen(PORT, () => console.log(`Troop server has started. Listening on port ${PORT}`))
+server.listen(PORT, () => console.log(`Troop server has started. Listening on port ${PORT}`));
